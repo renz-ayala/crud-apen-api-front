e90db44f5a057353e2eb8057d1164c9a670091d4
renz.ayala <renzayala98@gmail.com>
2026-03-22 17:12:18 -0500

    fix router.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ConsultarPlacaComponent } from './pages/consultar-placa/consultar-placa.component';
-
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { ApiModule, Configuration } from './api';
 import { environment } from '../environments/environment';
+import { provideHttpClient } from '@angular/common/http'
+import { AppComponent } from './app.component';
 
 export function apiConfig() {
   return new Configuration({
@@ -18,17 +15,16 @@ export function apiConfig() {
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ConsultarPlacaComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-     FormsModule,
-    HttpClientModule,
+    FormsModule,
     ApiModule.forRoot(apiConfig)
   ],
   providers: [
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners()
   ],
   bootstrap: [AppComponent]
